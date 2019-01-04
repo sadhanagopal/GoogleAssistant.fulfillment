9d1bb7e711d619388114d1ef4911b6aeb9d8e044
Sadhana Gopal <sgopal@independer.nl>
2019-01-04 10:37:19 +0100

    Added google assistant project

---
+++ GoogleAssistant.fulfillment/Controllers/WebhookController.cs
@@ -0,0 +1,28 @@
+﻿using Google.Apis.Dialogflow.v2.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GoogleAssistant.fulfillment.Controllers {
+  [Route("api/[controller]")]
+  [ApiController]
+  public class WebhookController : ControllerBase {
+
+    [HttpGet]
+    public ActionResult<string> Get() {
+      return "v0.0.0.1";
+    }
+
+    // POST api/webhook
+    [HttpPost]
+    public ActionResult<GoogleCloudDialogflowV2WebhookResponse> Post([FromBody] GoogleCloudDialogflowV2WebhookRequest request) {
+      
+      //request.QueryResult.Parameters      
+      //DialogflowBaseServiceRequest req = new 
+
+      var response = new GoogleCloudDialogflowV2WebhookResponse() {
+        FulfillmentText = "Hello from " + request.QueryResult.Intent.DisplayName
+      };
+
+      return response;
+    }
+  }
+}
